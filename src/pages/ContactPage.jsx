ed78a3b2a12bd3b6a7d42d2844ba36edb6043392
barbacba <fernando_areosa@hotmail.com.ar>
2024-02-23 04:17:15 -0300

    fix (index.html, src/index.css, src/pages/ContactPage.jsx) fix ui bugs

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,33 +1,32 @@
-import React from 'react';
-import { Container, Typography, CssBaseline, Paper, Box } from '@mui/material';
-import ContactForm from '../components/ContactForm';
+import React from "react";
+import { Container, Typography, CssBaseline, Paper, Box } from "@mui/material";
+import ContactForm from "../components/ContactForm";
 
 const ContactPage = () => {
-	return (
-		<Container component='main' maxWidth='xs'>
-			<Box
-				width='100vw'
-				display='flex'
-				flexDirection='column'
-				alignItems='center'
-			>
-				<CssBaseline />
-				<Paper
-					elevation={3}
-					component={Box}
-					p={3}
-					display='flex'
-					flexDirection='column'
-					alignItems='center'
-				>
-					<Typography variant='h2' component='h2' gutterBottom>
-						Contacto
-					</Typography>
-					<ContactForm />
-				</Paper>
-			</Box>
-		</Container>
-	);
+  return (
+    <Container component="main" maxWidth="100vw">
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        height="100vh"
+      >
+        <CssBaseline />
+        <Paper elevation={3} component={Box} p={3}>
+          <Typography
+            variant="h3"
+            component="h2"
+            gutterBottom
+            alignSelf="center"
+          >
+            Contacto
+          </Typography>
+          <ContactForm />
+        </Paper>
+      </Box>
+    </Container>
+  );
 };
 
 export default ContactPage;
